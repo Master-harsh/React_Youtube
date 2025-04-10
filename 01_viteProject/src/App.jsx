d4d3687586a836a5536793efac6e330ef1cde1d7
Master-harsh <harshtiwari7479@gmail.com>
2025-04-10 17:04:55 +0530

    creation of component

--- 01_viteProject/src/App.jsx
+++ 01_viteProject/src/App.jsx
@@ -1,10 +1,9 @@
+import Chai from "./Chai";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello Harsh welcome to React</h1>
-        <p>This is a new paragraph.</p>
-      </div>
+      <Chai />
     </>
   )
 }
---
+++ 01_viteProject/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {
+    return (
+        <div>
+            <h1>Yes sir!</h1>
+            <h3>Chai is ready</h3>
+        </div>
+    );
+}
+
+export default Chai;
