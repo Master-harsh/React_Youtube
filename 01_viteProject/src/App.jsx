7d00ea558a0973a4180e44640853efd8369d5391
Master-harsh <harshtiwari7479@gmail.com>
2025-04-11 17:00:02 +0530

    react new

--- 01_viteProject/src/App.jsx
+++ 01_viteProject/src/App.jsx
@@ -1,9 +1,12 @@
 import Chai from "./Chai";
 
 function App() {
+  const username = "Chai";
+
   return (
     <>
       <Chai />
+      <h1>Chai aur react {username}</h1>
     </>
   )
 }
--- 01_viteProject/src/main.jsx
+++ 01_viteProject/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp (){
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    click me to visit google
+  </a>
+);
+
+const anotherUser = "Chai aur React";
+
+const reacrElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit google',
+  anotherUser
+);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    reacrElement
+
 )
+
